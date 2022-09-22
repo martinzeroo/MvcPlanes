51f6d0c32f76283a646716850434cdf67356f23f
Martin <martinhbesalu@gmail.com>
2022-09-22 14:17:38 +0300

    Added a wiew

--- MvcPlanes/Controllers/HelloWorldController.cs
+++ MvcPlanes/Controllers/HelloWorldController.cs
@@ -5,25 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace MvcPlanes.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
         public IActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-        }
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
+            return View();
+        }
     }
 }

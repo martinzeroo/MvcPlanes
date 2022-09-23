df85cfd77ca7a8b39a345576927954a7c89004a7
Martin <martinhbesalu@gmail.com>
2022-09-23 10:22:02 +0300

    Controller actions and views

--- MvcPlanes/Models/Plane.cs
+++ MvcPlanes/Models/Plane.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcPlane.Models
 {
@@ -8,9 +9,11 @@ namespace MvcPlane.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime Departure { get; set; }
         public string Model { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }

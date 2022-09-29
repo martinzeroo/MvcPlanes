9e357d6f65deb91c64026e560655a1348115f541
Martin <martinhbesalu@gmail.com>
2022-09-29 13:55:56 +0300

    Add validation

--- MvcPlanes/Models/Plane.cs
+++ MvcPlanes/Models/Plane.cs
@@ -9,15 +9,26 @@ namespace MvcPlane.Models
     public class Plane
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
 
-        [Display(Name = "Release Date")]
+        [Display(Name = "Departure")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Departure { get; set; }
+
+        
+        [Required]
+        [StringLength(30)]
         public string Model { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [Required]
         public string Speed { get; set; }
 
     }
